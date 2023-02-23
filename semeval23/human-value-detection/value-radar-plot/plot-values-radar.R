c9b22df9056d71281ae9e3dc7f09befbfd85fd27
Johannes Kiesel <johannes.kiesel@uni-weimar.de>
2023-02-23 11:54:21 +0100

    values radar working

--- semeval23/human-value-detection/value-radar-plot/plot-values-radar.R
+++ semeval23/human-value-detection/value-radar-plot/plot-values-radar.R
@@ -1,5 +1,19 @@
+#!/usr/bin/env Rscript
+
+args = commandArgs(trailingOnly=TRUE)
+
+data <- read.csv(args[1], sep="\t", header=FALSE)
+output <- "radar-plot.pdf"
+if (length(args) > 1) {
+  output <- args[2]
+}
+
+
 library("grImport")
-radar <- readPicture("radar.ps.xml")[-1] # created by 'pdftops radar.pdf' and 'PostScriptTrace("radar.ps")'
+radar <- readPicture("values-radar-empty.ps.xml")[-1]
+# created by:
+# $ pdftops values-radar-empty.ai values-radar-empty.ps
+# $ Rscript -e 'library("grImport");PostScriptTrace("values-radar-empty.ps")'
 
 radar.line <- function(values, col="black", pch=21, cex=0.5) {
   values.extended <- c(values, values[1])
@@ -16,7 +30,7 @@ radar.line <- function(values, col="black", pch=21, cex=0.5) {
       r <- values.extended[i] * radius + radius.start
       px <- r * cos((start - i * step) * pi / 180) - xcorrection
       py <- r * sin((start - i * step) * pi / 180) - ycorrection
-      points(x=px, y=py, col=col, bg=col, pch=pch, cex=cex)
+      # points(x=px, y=py, col=FALSE, bg=col, pch=pch, cex=cex)
       if (!is.na(values.extended[i+1])) {
         r2 <- values.extended[i+1] * radius + radius.start
         px2 <- r2 * cos((start - (i+1) * step) * pi / 180) - xcorrection
@@ -27,11 +41,17 @@ radar.line <- function(values, col="black", pch=21, cex=0.5) {
   }
 }
 
-pdf("radar-plotted.pdf", width=4, height=4)
+radar.lines <- function(data, ...) {
+  colors <- data[,1]
+  values <- as.matrix(suppressWarnings(sapply(data[,-1], as.numeric)))
+  for (i in 1:length(colors)) {
+    radar.line(values[i,], col=colors[i], ...)
+  }
+}
+
+pdf(output, width=4, height=4)
 par(mar=c(0,0,0,0), oma=c(0,0,0,0))
 plot(0, xlim=c(-1,1), ylim=c(-1,1), type="n", bty="n", axes=FALSE)
 grid.picture(radar)
-radar.line(rep(0.0, 20), "blue")
-radar.line(rep(c(0.1, 0.2, NA, 0.4), 5), "red")
-radar.line(rep(1, 20))
+radar.lines(data)
 dev.off()
