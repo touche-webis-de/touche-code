488af14fc7bc6a327ad208e52737a2989b72eb97
Nicolas Handke <nicolas.handke@gmx.de>
2024-02-06 01:26:50 +0100

    Update conversion script

--- clef24/human-value-detection/inception-data-conversion/convert_curation.py
+++ clef24/human-value-detection/inception-data-conversion/convert_curation.py
@@ -10,6 +10,7 @@ import os
 
 import zipfile
 from trankit import Pipeline
+from tqdm import tqdm
 
 import pandas as pd
 
@@ -31,24 +32,43 @@ _value_dictionary = {value.replace(':', ' -').upper(): value for value in _value
 ######################
 
 
+class NoSpanEntryError(KeyError):
+    pass
+
+
 ################################
 # START: Trankit Functions #####
 ################################
 
-_lang_codes = {'EN': 'english', 'FR': 'french', 'EL': 'greek'}
+_lang_codes = {
+    'BG': 'bulgarian',
+    'DE': 'german',
+    'EL': 'greek',
+    'EN': 'english',
+    'FR': 'french',
+    'HE': 'hebrew',
+    'IT': 'italian',
+    'NL': 'dutch',
+    'TR': 'turkish'
+}
 _lang_pipelines = {}
 
 
-def _get_pipeline_for_file(text_id: str) -> Optional[Pipeline]:
-    detected_language = None
+def _detect_language(text_id: str, verbose: bool = False) -> Optional[str]:
     for code, language in _lang_codes.items():
         if text_id.upper().startswith(code):
-            print(f'Detected language {language} for TEXT-ID {text_id}')
-            detected_language = language
-            break
-    if detected_language is None:
+            if verbose:
+                print(f'Detected language {language} for TEXT-ID {text_id}')
+            return language
+    if verbose:
         print(f'Unable to detect language for file "{text_id}".')
-        return None
+    return None
+
+
+def _get_pipeline_for_file(text_id: str, verbose: bool = False) -> "tuple[Optional[Pipeline], str]":
+    detected_language = _detect_language(text_id=text_id, verbose=verbose)
+    if detected_language is None:
+        return None, ""
 
     global _lang_pipelines
     if detected_language in _lang_pipelines.keys():
@@ -58,7 +78,7 @@ def _get_pipeline_for_file(text_id: str) -> Optional[Pipeline]:
     else:
         pipeline = Pipeline(lang=detected_language)
         _lang_pipelines[detected_language] = pipeline
-    return pipeline
+    return pipeline, detected_language
 
 
 def _compare_entries(entry_sentence: Dict, entry_trankit: Dict) -> Dict:
@@ -110,15 +130,22 @@ def _find_file_by_name(current_path: str, target_file: str) -> List[str]:
     return []
 
 
-def convert_to_records(path: str):
+def convert_to_records(path: str, pipeline: Pipeline = None, verbose: bool = False, progress=None):
     text_id = os.path.basename(os.path.dirname(path)).upper()[:-4]
 
     with open(path, 'r') as file:
         json_data = json.load(file)
 
     full_text = str(json_data['_referenced_fss']['1']['sofaString'])
-    annotations: List = json_data['_views']['_InitialView']['Span']
-    annotations.sort(key=lambda x: (x.get('begin', 0), x['end']))
+    if 'Span' not in json_data['_views']['_InitialView'].keys():
+        annotations = []
+        if progress is None:
+            print(f'Missing "Span" entry in {path}')
+        else:
+            progress.write(f'Missing "Span" entry in {path}')
+    else:
+        annotations: List = json_data['_views']['_InitialView']['Span']
+        annotations.sort(key=lambda x: (x.get('begin', 0), x['end']))
     sentence_data = json_data['_views']['_InitialView']['Sentence']
 
     len_full_text = len(full_text)
@@ -130,7 +157,10 @@ def convert_to_records(path: str):
     ground_truth_label_records = []
     value_records = []
 
-    trankit_pipeline = _get_pipeline_for_file(text_id)
+    if pipeline is None:
+        trankit_pipeline, _ = _get_pipeline_for_file(text_id)
+    else:
+        trankit_pipeline = pipeline
     sentence_data_trankit = []
     if trankit_pipeline is not None:
         sentence_data_trankit = [
@@ -165,7 +195,7 @@ def convert_to_records(path: str):
         selected_entry = _compare_entries(entry_sentence, entry_trankit)
         if selected_entry is None:
             if len(full_text[index_full_text:].strip()) > 0:
-                sys.exit(f'No valid sentence for document "{path}" past index {index_full_text}.')
+                raise ValueError(f'No valid sentence for document "{path}" past index {index_full_text}.')
             else:
                 break
 
@@ -177,34 +207,46 @@ def convert_to_records(path: str):
         sentence_start_end = (selected_entry['begin'], selected_entry['end'])
 
         # Process annotations
-        if annotations and sentence_start_end[0] > annotations[0].get('begin', 0):
-            if 'overlap' in annotations[0].keys():
-                annotations[0]['begin'] = sentence_start_end[0]
-            else:
-                sys.exit(f'Skipped annotation "{annotations[0]}" in "{path}".')
-        while annotations and \
-                sentence_start_end[0] <= annotations[0].get('begin', 0) < sentence_start_end[1]:
-            annotation = annotations.pop(0)
+        annotations_index = 0
+        while annotations and annotations_index < len(annotations):
+            if 'overlap' in annotations[annotations_index].keys() and \
+                    sentence_start_end[0] > annotations[annotations_index].get('begin', 0):
+                annotations[annotations_index]['begin'] = sentence_start_end[0]
+            if not (sentence_start_end[0] <= annotations[annotations_index].get('begin', 0) < sentence_start_end[1]):
+                annotations_index += 1
+                continue
+            annotation = annotations.pop(annotations_index)
+            annotations_index -= 1
 
             # Apply overlapping value to all touched sentences
             if annotation['end'] > sentence_start_end[1]:
-                if 'trankit' in selected_entry.keys():
-                    print(f"Value \"{annotation}\" across sentence borders (due to trankit) for file \"{path}\"")
-                else:
-                    print(f"Value \"{annotation}\" across sentence borders for file \"{path}\"")
+                if verbose:
+                    if 'trankit' in selected_entry.keys():
+                        if progress is None:
+                            print(f"Value \"{annotation}\" across sentence borders (due to trankit) for file \"{path}\"")
+                        else:
+                            progress.write(f"Value \"{annotation}\" across sentence borders (due to trankit) for file \"{path}\"")
+                    else:
+                        if progress is None:
+                            print(f"Value \"{annotation}\" across sentence borders for file \"{path}\"")
+                        else:
+                            progress.write(f"Value \"{annotation}\" across sentence borders for file \"{path}\"")
                 new_annotation = copy.deepcopy(annotation)
                 new_annotation['overlap'] = True
                 new_annotation['begin'] = sentence_start_end[1]
                 annotation['end'] = sentence_start_end[1]
                 annotations.insert(0, new_annotation)
-                annotations.sort(key=lambda x: (x.get('begin', 0), x['end']))
+                annotations_index += 1
 
             value_start = annotation.get('begin', 0) - sentence_start_end[0]
             value_end = annotation['end'] - sentence_start_end[0]
 
             value_name = _value_dictionary.get(str(annotation['label']).upper(), 'NaN')
             if value_name == 'NaN':
-                print(f'Error on {text_id}-{sentence_id} {value_start}:{value_end}')
+                if progress is None:
+                    print(f'Error on {text_id}-{sentence_id} {value_start}:{value_end}')
+                else:
+                    progress.write(f'Error on {text_id}-{sentence_id} {value_start}:{value_end}')
                 continue
             if 'Attainment' in annotation.keys():
                 attainment = str(annotation['Attainment'])[:-2].strip()
@@ -216,6 +258,11 @@ def convert_to_records(path: str):
                 {_text_id_ref: text_id, _sentence_id_ref: sentence_id, _label_ref: value_name,
                  _attainment_ref: attainment, 'Begin': value_start, 'End': value_end}
             )
+
+            annotations_index += 1
+
+        # pre-sort annotations for next sentence
+        annotations.sort(key=lambda x: (x.get('begin', 0), x['end']))
 
         ground_truth_records.append(ground_truth_record)
         ground_truth_label_record = {_text_id_ref: text_id, _sentence_id_ref: sentence_id}
@@ -244,25 +291,57 @@ def parse_args():
 
     parser.add_argument('-i', '--input-file', type=str, required=True, help='The input file (e.g., zip-Archive) that contains or is "CURATION_USER.json"')
     parser.add_argument('-o', '--output-dir', type=str, required=False, help='Directory for final tsv-files (default: same directory as for the input-file)')
+    parser.add_argument('-v', '--verbose', action='store_true', required=False,
+                        help='Whether to print intermediate info, e.g. values across sentence borders (default: False)')
 
     return parser.parse_args()
 
 
-def main(input_file: str, output_dir: str):
+def main(input_file: str, output_dir: str, verbose: bool = False):
     curation_files = _find_file_by_name(input_file, 'CURATION_USER.json')
 
     sentence_records, ground_truth_records, ground_truth_label_records, value_records = [], [], [], []
 
+    batches = {}
+
+    print("==================================================\n"
+          "Detecting present languages\n"
+          "==================================================")
     for file in curation_files:
-        try:
-            new_sentence_records, new_ground_truth_records, new_ground_truth_label_records, new_value_records = convert_to_records(file)
-            sentence_records += new_sentence_records
-            ground_truth_records += new_ground_truth_records
-            ground_truth_label_records += new_ground_truth_label_records
-            value_records += new_value_records
-        except KeyError as e:
-            print(file)
-            raise e
+        # get text_id from file path
+        text_id = os.path.basename(os.path.dirname(file)).upper()[:-4]
+
+        language = _detect_language(text_id)
+        if language is not None:
+            if language in batches.keys():
+                batches[language].append((text_id, file))
+            else:
+                batches[language] = [(text_id, file)]
+        else:
+            print(f'Unable to detect language for file "{text_id}".')
+
+    for language, batch in batches.items():
+        print("==================================================\n"
+              f"Processing {len(batch)} files for {language}\n"
+              "==================================================")
+        pipeline, _ = _get_pipeline_for_file(batch[0][0])
+        with tqdm(total=len(batch), desc=f'{language}', disable=False) as progress:
+            for text_id, file in batch:
+                try:
+                    new_sentence_records, new_ground_truth_records, new_ground_truth_label_records, new_value_records = \
+                        convert_to_records(file, pipeline=pipeline, verbose=verbose, progress=progress)
+                    sentence_records += new_sentence_records
+                    ground_truth_records += new_ground_truth_records
+                    ground_truth_label_records += new_ground_truth_label_records
+                    value_records += new_value_records
+                except NoSpanEntryError as nSEE:
+                    progress.write(str(nSEE))
+                except ValueError as vE:
+                    progress.write(str(vE))
+                except KeyError as e:
+                    print(file)
+                    raise e
+                progress.update()
 
     pd.DataFrame.from_records(sentence_records).sort_values(by=[_text_id_ref, _sentence_id_ref]).to_csv(
         os.path.join(output_dir, 'sentences.tsv'),
@@ -288,4 +367,4 @@ if __name__ == '__main__':
         output_dir = os.path.dirname(args.input_file)
     else:
         output_dir = args.output_dir
-    main(input_file=args.input_file, output_dir=output_dir)
+    main(input_file=args.input_file, output_dir=output_dir, verbose=args.verbose)
