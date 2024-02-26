8d90107e89443ab29a6bb01b3937b2d9541ab8a6
Johannes Kiesel <johannes.kiesel@uni-weimar.de>
2024-02-26 23:00:30 +0100

    update

--- clef24/human-value-detection/approaches/bert-baseline-dev/bert_baseline.py
+++ clef24/human-value-detection/approaches/bert-baseline-dev/bert_baseline.py
@@ -1,4 +1,5 @@
 import os
+import datasets
 import pandas
 import numpy
 import torch
@@ -8,57 +9,79 @@ import tempfile
 
 # GENERIC
 
-class MultiLabelTrainer(transformers.Trainer):
-    """
-        A transformers `Trainer` with custom loss computation
+values = [ "Self-direction: thought", "Self-direction: action", "Stimulation",  "Hedonism", "Achievement", "Power: dominance", "Power: resources", "Face", "Security: personal", "Security: societal", "Tradition", "Conformity: rules", "Conformity: interpersonal", "Humility", "Benevolence: caring", "Benevolence: dependability", "Universalism: concern", "Universalism: nature", "Universalism: tolerance" ]
+labels = sum([[value + " attained", value + " constrained"] for value in values], [])
+id2label = {idx:label for idx, label in enumerate(labels)}
+label2id = {label:idx for idx, label in enumerate(labels)} 
 
-        Methods
-        -------
-        compute_loss(model, inputs, return_outputs=False):
-            Overrides loss computation from Trainer class
-        """
-    def compute_loss(self, model, inputs, return_outputs=False):
-        """Custom loss computation"""
-        labels = inputs.pop("labels")
-        outputs = model(**inputs)
-        logits = outputs.logits
-        loss_fct = torch.nn.BCEWithLogitsLoss()
-        loss = loss_fct(logits.view(-1, self.model.config.num_labels),
-                        labels.float().view(-1, self.model.config.num_labels))
-        return (loss, outputs) if return_outputs else loss
 
-def compute_metrics(eval_prediction, value_classes):
-    """Custom metric calculation function for MultiLabelTrainer"""
+# tokenizer = transformers.AutoTokenizer.from_pretrained(pretrained_model)
+
+def load_dataset(directory, tokenizer, load_labels=True):
+    sentences_file_path = os.path.join(directory, "sentences.tsv")
+    labels_file_path = os.path.join(directory, "labels.tsv")
+    
+    data_frame = pandas.read_csv(sentences_file_path, encoding="utf-8", sep="\t", header=0)
+    encoded_sentences = tokenizer(data_frame["Text"], truncation=True)
+
+    if load_labels and os.path.isfile(labels_file_path):
+        labels_frame = pandas.read_csv(labels_file_path, encoding="utf-8", sep="\t", header=0)
+        labels_frame = pandas.merge(data_frame, labels_frame, on=["Text-ID", "Sentence-ID"])
+        labels_matrix = np.zeros((labels_frame.shape[0], len(labels)))
+        for idx, label in enumerate(labels):
+            if label in labels_frame.columns:
+                labels_matrix[:, idx] = (labels_frame[label] >= 0.5).astype(int)
+        encoded_sentences["labels"] = labels_matrix.tolist()
+
+    encoded_sentences = datasets.Dataset.from_dict(encoded_sentences)
+    return encoded_sentences, data_frame["Text-ID"], data_frame["Sentence-ID"]
+
+
+def compute_metrics(eval_prediction):
     prediction_scores, label_scores = eval_prediction
     predictions = prediction_scores >= 0.5
     labels = label_scores >= 0.5
 
     f1_scores = {}
-    for i, v in enumerate(value_classes):
-        predicted = predictions.sum()
-        true = labels.sum()
-        true_positives = numpy.logical_and(predictions, labels).sum()
+    for i in range(eval_prediction.shape[1]):
+        predicted = predictions[:,i].sum()
+        true = labels[:,i].sum()
+        true_positives = numpy.logical_and(predictions[:,i], labels[:,i]).sum()
         precision = 0 if predicted == 0 else true_positives / predicted
         recall = 0 if true == 0 else true_positives / true
-        f1_scores[v] = round(0 if precision + recall == 0 else 2 * (precision * recall) / (precision + recall), 2)
+        f1_scores[i] = round(0 if precision + recall == 0 else 2 * (precision * recall) / (precision + recall), 2)
     f1_scores['avg-f1-score'] = round(numpy.mean(list(f1_scores.values())), 2)
 
     return {'f1-score': f1scores, 'marco-avg-f1score': f1scores['avg-f1-score']}
 
-def train(batch_size=8, num_train_epochs=20):
-    """Train a model"""
+
+def train(training_dataset, validation_dataset, pretrained_model="bert-base-uncased", tokenizer = tokenizer, batch_size=8, num_train_epochs=20, learning_rate=2e-5, weight_decay=0.01):
     output_dir = tempfile.TemporaryDirectory()
     args = transformers.TrainingArguments(
         output_dir=output_dir,
         evaluation_strategy="steps",
-        learning_rate=2e-5,
+        learning_rate=learning_rate,
         per_device_train_batch_size=batch_size,
         per_device_eval_batch_size=batch_size,
         num_train_epochs=num_train_epochs,
-        weight_decay=0.01,
+        weight_decay=weight_decay,
         load_best_model_at_end=True,
         metric_for_best_model='marco-avg-f1score'
     )
+
+    model = transformers.AutoModelForSequenceClassification.from_pretrained(
+        pretrained_model, problem_type="multi_label_classification",
+        num_labels=len(labels), id2label=id2label, label2id=label2id)
+    if torch.cuda.is_availabled():
+        model = model.to('cuda')
+
+    trainer = transformers.Trainer(model, args,
+        train_dataset=training_dataset, eval_dataset=validation_dataset,
+        compute_metrics=compute_metrics, tokenizer=tokenizer)
+    trainer.train()
+    trainer.evaluate()
+
+    return model
 
 
 
