a50fcd06f699fedad2e1c3053e7ca9d510277b2a
Johannes Kiesel <johannes.kiesel@uni-weimar.de>
2024-07-04 09:37:26 +0200

    add random baseline

--- clef24/human-value-detection/value-radar-plot/plot-values-radar.R
+++ clef24/human-value-detection/value-radar-plot/plot-values-radar.R
@@ -15,8 +15,8 @@ colors <- data.full[,2]
 data <- data.full[,3:21]
 
 dims <- 19
-radar.max <- 0.7
-radar.segments <- 7
+radar.max <- 0.75
+radar.segments <- 3 
 
 names.outer <- list(c(1.5, "Self-direction:"), c(3, "Stimulation"), c(4, "Hedonism"), c(5, "Achievement"), c(6.5, "Power:"), c(8, "Face"), c(9.5, "Security:"), c(11, "Tradition"), c(12.5, "Conformity:"), c(14, "Humility"), c(15.5, "Benevolence:"), c(18, "Universalism:"))
 names.inner <- list(c(1, "thought"), c(2, "action"), c(6, "dominance"), c(7, "resources"), c(9, "personal"), c(10, "societal"), c(12, "rules"), c(13, "interpersonal"), c(15, "caring"), c(16, "dependability"), c(17, "concern"), c(18, "nature"), c(19, "tolerance"))
@@ -47,7 +47,7 @@ arc.segments <- function (lines, ...) {
   }
 }
 
-axis.labels <- sprintf("%.1f", 0:radar.segments * (radar.max/radar.segments))
+axis.labels <- sprintf("%.2f", 0:radar.segments * (radar.max/radar.segments))
 
 cairo_pdf(output, width=5, height=5)
 par(mar=c(-0,0,0,0), oma=c(0,0,0,0), family="Helvetica")
