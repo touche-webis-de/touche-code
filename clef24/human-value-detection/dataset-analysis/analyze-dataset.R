0dc6187acb7659783f5bcd63d23f4f5c4c005739
Johannes Kiesel <johannes.kiesel@uni-weimar.de>
2024-10-03 20:46:47 +0200

    update colors

--- clef24/human-value-detection/dataset-analysis/analyze-dataset.R
+++ clef24/human-value-detection/dataset-analysis/analyze-dataset.R
@@ -2,6 +2,12 @@
 
 values <- c("Self-direction\nthought", "Self-direction\naction", "Stimulation", "Hedonism", "Achievement", "Power\ndominance", "Power\nresources", "Face", "Security\npersonal", "Security\nsocietal", "Tradition", "Conformity\nrules", "Conformity\ninterpersonal", "Humility", "Benevolence\ncaring", "Benevolence\ndependability", "Universalism\nconcern", "Universalism\nnature", "Universalism\ntolerance")
 
+col.constrained="#db58d3"
+col.combined="#58dbab"
+col.attained="#db5992"
+col.news=col.attained
+col.manifestos=col.constrained
+
 args <- commandArgs(trailingOnly=TRUE)
 data.filename <- args[1]
 
@@ -12,9 +18,10 @@ data$Is.manifesto <- regexpr("_M_", data$Text.ID) != -1
 plot.barsperlanguage <- function(filename, data, ...) {
   pdf(filename, width=7, height=6)
   par(mar=c(4, 4, 0.4, 0.1))
-  barplot(t(table(data[c("Language","Is.manifesto")]))[c(2,1),], xlab="Language", las=1, col=c("orange", "blue"), ...)
+  barplot(t(table(data[c("Language","Is.manifesto")]))[c(2,1),], xlab="", las=1, col=rev(c(col.combined, col.manifestos)), ...)
+  mtext("Language", side=1, line=2.5)
   grid(nx=NA, ny=NULL)
-  legend("topleft", legend=c("All", "Manifestos"), fill=c("blue", "orange"), bty="n")
+  legend("topleft", legend=c("All", "Manifestos"), fill=c(col.combined, col.manifestos), bty="n")
   dev.off()
 }
 
@@ -25,19 +32,27 @@ plot.barsperlanguage("files-per-language.pdf", data[data$Sentence.ID == 1,], yla
 plot.barsperlanguage("sentences-per-language.pdf", data, ylab="Sentences")
 
 # fraction of sentences with at least one value
-col <- rev(c("#edf8fb", "#ccece6", "#99d8c9", "#66c2a4", "#2ca25f", "#006d2c"))
+col <- rev(c("white", "#edf8fb", "#ccece6", "#99d8c9", "#66c2a4", "#2ca25f", "#006d2c"))
+col <- rev(c("white", "#d3db57", "#92db59", "#59db61", "#59d3db", "#6159db", "#a259db"))
+plot.fraction.sentences <- function(ylim.max, box.ytop=ylim.max) {
+  par(mar=c(4, 4, 0.4, 0.1))
+  barplot(t(sapply(rev(0:6), function(x) {return(table(data[rowSums(data[3:40]) == x,]$Language) / table(data$Language))})), xlab="", ylab="Percentage of sentences with at least that many values", yaxt="n", col=col, ylim=c(0,ylim.max))
+  mtext("Language", side=1, line=2.5)
+  at <- (0:5)/5*ylim.max
+  labels <- sprintf("%.1f%%", at*100)
+  if (at[2] * 100 >= 1) {
+    labels <- sprintf("%d%%", at*100)
+  }
+  axis(2, at=at, labels=labels, line=-0.25, las=1)
+  grid(nx=NA, ny=NULL)
+  rect(xleft=0, xright=11, ybottom=-0.0001, ytop=box.ytop+0.0001, lwd=2, xpd=TRUE, border="orange")
+  legend("topright", legend=0:6, fill=rev(col), bg="white", box.col="white")
+}
 pdf("fraction-sentences-with-value-per-language.pdf", width=7, height=6)
-par(mar=c(4, 4, 0.4, 0.1))
-barplot(t(sapply(rev(1:6), function(x) {return(table(data[rowSums(data[3:40]) == x,]$Language) / table(data$Language))})), xlab="Language", ylab="Fraction of sentences with at least that many values", las=1, col=col, ylim=c(0,1))
-legend("topleft", legend=1:6, fill=rev(col), bty="n")
-grid(nx=NA, ny=NULL)
+plot.fraction.sentences(1, box.ytop=0.025)
 dev.off()
-
 pdf("fraction-sentences-with-value-per-language-zoomed.pdf", width=7, height=6)
-par(mar=c(4, 4, 0.4, 0.1))
-barplot(t(sapply(rev(1:6), function(x) {return(table(data[rowSums(data[3:40]) == x,]$Language) / table(data$Language))})), xlab="Language", ylab="Fraction of sentences with at least that many values", las=1, col=col, ylim=c(0,0.005))
-legend("topleft", legend=1:6, fill=rev(col), bty="n")
-grid(nx=NA, ny=NULL)
+plot.fraction.sentences(0.025)
 dev.off()
 
 
@@ -53,11 +68,11 @@ plot.histsmallperlanguage <- function(filename.base, data, subset=rep(TRUE, dim(
     hist.all <- hist(table(sentences.all[subset[data$Language == language]]), breaks=(-1:sentences.max)+0.5, plot=FALSE)
     hist.manifestos <- hist(table(sentences.manifestos[subset[data$Language == language & data$Is.manifesto]]), breaks=(-1:sentences.max)+0.5, plot=FALSE)
 
-    plot(hist.all, col="blue", xlim=c(0-0.5, sentences.max+0.5), main=NULL, ylim=c(0, 50), ...)
-    plot(hist.manifestos, col="orange", xlim=c(0-0.5, sentences.max+0.5), add=TRUE)
+    plot(hist.all, col=col.combined, xlim=c(0-0.5, sentences.max+0.5), main=NULL, ylim=c(0, 50), ...)
+    plot(hist.manifestos, col=col.manifestos, xlim=c(0-0.5, sentences.max+0.5), add=TRUE)
     grid()
     text(7, 45, language, cex=2)
-    legend("topright", legend=c("All", "Manifestos"), fill=c("blue", "orange"), bty="n")
+    legend("topright", legend=c("All", "Manifestos"), fill=c(col.combined, col.manifestos), bty="n")
     dev.off()
   }
 }
@@ -77,9 +92,9 @@ plot.fractionsentencespervalue <- function(sentences, genre, language) {
   filename <- paste(filename.base, "-", tolower(genre), "-", tolower(language), ".pdf", sep="")
   pdf(filename, width=7, height=3)
   par(mar=c(0.3, 4, 0.4, 0.1))
-  plot(1:19, sums.attained / sentences.num, type="b", col="green", pch=3, xlab="", xaxt="n", ylab="Sentences with value", las=2, ylim=c(0,0.3))
-  lines(1:19, sums.constrained / sentences.num, type="b", col="red", pch=4)
-  lines(1:19, (sums.attained + sums.constrained) / sentences.num, type="b", col="blue", pch=8)
+  plot(1:19, sums.attained / sentences.num, type="b", col=col.attained, pch=3, xlab="", xaxt="n", ylab="Sentences with value", las=2, ylim=c(0,0.3))
+  lines(1:19, sums.constrained / sentences.num, type="b", col=col.constrained, pch=4)
+  lines(1:19, (sums.attained + sums.constrained) / sentences.num, type="b", col=col.combined, pch=8)
   grid(nx=NA, ny=NULL)
   abline(v=1:19, col="lightgray", lty="dotted")
   text(1, 0.29, paste(genre, language), cex=1, adj=0)
