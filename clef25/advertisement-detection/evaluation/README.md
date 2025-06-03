b44e1b2e01198561c2c6e7e4901757363961ef53
Maik Froebe <maik.froebe@uni-jena.de>
2025-06-03 22:10:09 +0200

    mf

---
+++ clef25/advertisement-detection/evaluation/README.md
@@ -0,0 +1,12 @@
+Run via:
+
+```
+tira-cli login --token ADMIN-TOKEN
+```
+
+```
+./evaluation-in-progress.py \
+    --task advertisement-in-retrieval-augmented-generation-2025 \
+    --datasets ads-in-rag-task-1-generation-test-20250506-test --datasets ads-in-rag-task-1-generation-test-20250603-test \
+    --output /mnt/ceph/storage/data-in-progress/data-research/web-search/TOUCHE-25/advertisement-in-retrieval-augmented-generation/submissions/ads-in-rag-task-1-generation
+```
--- clef25/advertisement-detection/evaluation/evaluation-in-progress.py
+++ clef25/advertisement-detection/evaluation/evaluation-in-progress.py
@@ -8,7 +8,7 @@ import json
 
 @click.command()
 @click.option("--task", type=click.Choice(["advertisement-in-retrieval-augmented-generation-2025"]), required=True, help="The task id in tira. See https://archive.tira.io/datasets?query=advertisement")
-@click.option("--datasets", type=click.Choice(["ads-in-rag-task-1-generation-test-20250506-test"]), multiple=True, help="The dataset id in tira. See https://archive.tira.io/datasets?query=advertisement")
+@click.option("--datasets", type=click.Choice(["ads-in-rag-task-1-generation-test-20250506-test", "ads-in-rag-task-1-generation-test-20250603-test"]), multiple=True, help="The dataset id in tira. See https://archive.tira.io/datasets?query=advertisement")
 @click.option("--output", type=str, required=True, help="The output directory.")
 def main(task, datasets, output):
     tira = Client()
